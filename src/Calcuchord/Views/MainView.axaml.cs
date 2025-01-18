3332454097e364b35608dcfc1ec02f9e93880e87
Thomas Kefauver <tkefauver@gmail.com>
2025-01-18 10:18:21 -0500

    Initial Commit

---
+++ src/Calcuchord/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Calcuchord.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Calcuchord/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Calcuchord.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
