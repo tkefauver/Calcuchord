1e537487cd45d1c507245961a42bfd598d4a6268
tkefauver <tkefauver@gmail.com>
2025-02-18 21:16:44 +0000

    Deploying to gh-pages from @ tkefauver/Calcuchord@f47496755df97e75c24e6f0a64ce32cbe5f72535 🚀

---
+++ js/prefs.js
@@ -0,0 +1,51 @@
+var isStorageLoaded = false;
+
+function initStorage() {
+    if (isStorageLoaded) {
+        return;
+    }
+    try {
+        localforage.config({
+            driver: localforage.INDEXEDDB,
+            //driver: localforage.WEBSQL, // Force WebSQL; same as using setDriver()
+            name: 'myApp',
+            version: 1.0,
+            size: 4980736, // Size of database, in bytes. WebSQL-only for now.
+            storeName: 'keyvaluepairs', // Should be alphanumeric, with underscores.
+            description: 'instrument tunings and bookmarks'
+        })
+        isStorageLoaded = true;
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+async function readPrefsAsync() {
+    initStorage();
+    try {
+        const value = await localforage.getItem('prefs');
+        // This code runs once the value has been loaded
+        // from the offline store.
+        return value;
+    } catch (err) {
+        // This code runs if there were any errors.
+        console.log(err);
+    }
+    return '';
+}
+
+function writePrefsAsync(prefsJson) {
+    initStorage();
+
+    localforage.setItem('prefs', prefsJson).then(function (value) {
+        // Do other things once the value has been saved.
+        console.log('saved:')
+        console.log(value);
+    }).catch(function (err) {
+        // This code runs if there were any errors
+        console.log('save error:');
+        console.log(err);
+        alert(err);
+    });
+}
