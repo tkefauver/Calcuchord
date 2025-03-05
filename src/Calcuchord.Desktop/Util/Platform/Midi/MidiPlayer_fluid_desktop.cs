18f2674dcf0da059a1118d44cac366d49be36d25
Thomas Kefauver <tkefauver@gmail.com>
2025-03-05 16:56:00 -0500

    fuzzy scoring added

--- src/Calcuchord.Desktop/Util/Platform/Midi/MidiPlayer_fluid_desktop.cs
+++ src/Calcuchord.Desktop/Util/Platform/Midi/MidiPlayer_fluid_desktop.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.IO;
 using NFluidsynth;
 using NFluidSettings = NFluidsynth.Settings;
 
@@ -38,13 +40,24 @@ namespace Calcuchord.Desktop {
         }
 
         protected override void PlayFile(string soundFontPath) {
-            Player player = new Player(Synth);
-            player.Add(MidiFilePath);
-            player.Play();
-            player.Join();
+            // Player player = new Player(Synth);
+            // player.Add(MidiFilePath);
+            // player.Play();
+            // player.Join();
+            if(!File.Exists(soundFontPath)) {
+                return;
+            }
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "fluidsynth",
+                Arguments = $"-a alsa -g 1.0 {soundFontPath} {MidiFilePath}",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+            using Process p = Process.Start(psi);
+
         }
-
-
     }
 
 }
