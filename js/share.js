bcd68c32c7d67118a5241c06fb9b1689941061b1
tkefauver <tkefauver@gmail.com>
2025-03-06 19:42:28 +0000

    Deploying to gh-pages from @ tkefauver/Calcuchord@e2f4115161b01d8bd377f2b212c80670e86d28ac 🚀

--- js/share.js
+++ js/share.js
@@ -1,53 +1,33 @@
-async function shareHtml(htmlString, title) {
-    // let success = false;
-    // let domParser = new DOMParser();
-    // let doc = domParser.parseFromString(htmlString, 'text/html');
-    try {
-        // let pdf_doc = new window.jspdf.jsPDF();
-        // var svgElements = doc.body.querySelectorAll('svg');
-        // svgElements.forEach(function (item) {
-        //     item.setAttribute("width", item.getBoundingClientRect().width);
-        //     item.setAttribute("height", item.getBoundingClientRect().height);
-        //     item.style.width = null;
-        //     item.style.height = null;
-        // });
-        // await pdf_doc.html(doc.documentElement.outerHTML);
-        // let base_64 = pdf_doc.output('datauristring').split('data:application/pdf;filename=generated.pdf;base64,')[1];
-        //
-        // const blob = new Blob([base_64], {type: 'application/pdf'});
-        // const file = new File([blob], title + ".pdf", {type: 'application/pdf'});
+async function shareFileAsync(b64, mimeType, fileName, title) {
+    const blob = new Blob([base64ToArrayBuffer(b64)], {type: mimeType});
+    const file = new File([blob], fileName, {type: mimeType});
 
-        const blob = new Blob([htmlString], {type: 'text/html'});
-        const file = new File([blob], 'shared_content.html', {type: 'text/html'});
-
-        if (navigator.share && navigator.canShare({files: [file]})) {
-            await navigator.share({
-                title: title,
-                files: [file],
-            });
-            console.log('Content shared successfully');
-            //success = true;
-        } else {
-            // pdf_doc.save(title + '.pdf');
-            // success = true;
-        }
-    } catch (error) {
-        console.error('Error sharing content:', error);
+    if (navigator.share && navigator.canShare({files: [file]})) {
+        await navigator.share({
+            title: title,
+            files: [file],
+        });
+        console.log(`${fileName} shared successfully`);
+        return true;
     }
-    // if (success) {
-    //     return;
-    // }
 
-    // let close_btn = doc.createElement('button');
-    // close_btn.setAttribute('class', 'close-btn');
-    // close_btn.setAttribute('onclick', 'window.parent.closeShare();');
-    // close_btn.InnerHtml = 'Close';
-    // doc.body.appendChild(close_btn);
-    //
-    // const iframe = document.createElement('iframe');
-    // iframe.srcdoc = doc.documentElement.outerHTML;
-    // iframe.setAttribute('class', "share");
-    // document.body.appendChild(iframe);
+    const uri = `data:${mimeType};base64,${b64}`;
+    const link = document.createElement('a');
+    link.href = uri;
+    link.download = fileName;
+    link.click();
+
+    console.log(`${fileName} share unsuccessfull`);
+    return false;
+}
+
+function base64ToArrayBuffer(base64) {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
 }
 
 function closeShare() {
